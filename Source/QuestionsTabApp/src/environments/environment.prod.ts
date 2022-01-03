90f06d8d2a4c444a67b8a43133dbdc8fc748426a
dsou-user <96622304+dsou-user@users.noreply.github.com>
2022-01-03 16:58:24 +0530

    Fix: Updated environment variables of questionapp

--- Source/QuestionsTabApp/src/environments/environment.prod.ts
+++ Source/QuestionsTabApp/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://solqbot.azurewebsites.net/api/Request/",
-    selfUrl: "https://solqbot-questions.azurewebsites.net",
+    apiBaseUrl: "https://qbotedudusol.azurewebsites.net/api/Request/",
+    selfUrl: "https://qbotedudusol-questions.azurewebsites.net",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
         tenantId: "d5c5977f-36d4-41a3-b226-4ad8f0e79bbc",
-        clientId: "783673f5-8103-4b78-a1a4-5d6ff2af3d2b",
+        clientId: "343f4ecb-863d-4ee0-983f-009b1bd3877b",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
